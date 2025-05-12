11653986e3e9574480281cce86fa10c10a481560
Brian <dpi.studio@gmail.com>
2025-05-12 10:45:05 +0800

    feat: 新增帳戶管理功能
    - 更新 `demoWML.csproj`，新增 `Microsoft.EntityFrameworkCore.Tools` 版本 `8.0.11` 的套件參考。
    - 新增 `AccountsController`，處理 `Account` 實體的 CRUD 操作。
    - 創建或修改多個 Razor 視圖以支援 `AccountsController`，包括：
      - `Create.cshtml`: 新增帳戶的表單。
      - `Delete.cshtml`: 刪除帳戶的確認頁面。
      - `Details.cshtml`: 顯示特定帳戶的詳細資訊。
      - `Edit.cshtml`: 編輯現有帳戶的表單。
      - `Index.cshtml`: 列出所有帳戶的頁面，並提供創建、編輯、查看詳細資訊或刪除的選項。
    - 所有視圖均強類型化為 `Account` 模型，並包含適當的 HTML 元素和驗證摘要，以確保良好的使用者體驗。

---
+++ demoWML/Controllers/AccountsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using demoWML.Models;
+
+namespace demoWML.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly WML_SD_A99Context _context;
+
+        public AccountsController(WML_SD_A99Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Accounts.ToListAsync());
+        }
+
+        // GET: Accounts/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Username,HashPassword,LastName,FirstName,AccountStatus,AccountType,LastUpdatedTime")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                account.Id = Guid.NewGuid();
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Username,HashPassword,LastName,FirstName,AccountStatus,AccountType,LastUpdatedTime")] Account account)
+        {
+            if (id != account.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(Guid id)
+        {
+            return _context.Accounts.Any(e => e.Id == id);
+        }
+    }
+}
